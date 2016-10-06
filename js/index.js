32131fd1725e188adf1aa3cb882ce15fc2de132f
Marc Shapiro <marcnshap@gmail.com>
2016-10-06 13:18:06 -0700

    Add comments and credits.  Change the computer's move if going first to allow any of the four corners.

--- js/index.js
+++ js/index.js
@@ -5,6 +5,23 @@ var emptyCount = 0;
 var gameOver = false;
 var cells = [[-1, -1, -1],[-1, -1, -1],[-1, -1, -1]];
 
+// I tried several methods of determining the best move for the computer before
+// I finally selected this one.  Each of my earlier methods worked fine to a point,
+// but then broke down into too many edge cases to be really workable.  This implimentation
+// uses a heuristic method to determine the best play, looking ahead two moves (the
+// computer's move and one player's move).  It is based on C code found at:
+//
+//     https://kartikkukreja.wordpress.com/2013/03/30/heuristic-function-for-tic-tac-toe/
+//
+// The author claims that this is unbeatable, and it may be.  I was unable to beat it,
+// myself, but I was able to play it to a draw more often than I should have.  This
+// was due to the fact that the strict heuristics had the computer move in the center
+// if it went first, instead of taking a corner.  This left four out of eight moves for the
+// human that could then be played to a draw.  By putting in a check for the first move and
+// then playing in an of the four corners if so, it reduces the human's moves which allow
+// playing to a draw from four to only one, thus incresing the computer's chance of a win
+// instead of only a draw.
+
 // Squares representing winning combinations
 var win_comb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
                     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
@@ -39,6 +56,21 @@ var clearBoard = function() {
   plays = 0;
 }
 
+// Set a status message and background color then blink the message three times.
+var setStatus = function(msg, color) {
+  $("#statusBar").css("background-color",color);
+  $("#statusBar").html(msg);
+  $("#statusBar").show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0);
+}
+
+// Set a status message and background color then blink the message three times and clear the board.
+var setStatusAndClear = function(msg, color) {
+  $("#statusBar").css("background-color",color);
+  $("#statusBar").html(msg);
+  $("#statusBar").show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0, clearBoard);
+}
+
+// Check if a cell is empty before allowing the human to play it.
 var chkEmpty = function(cell) {
   if (cell.html() !== "") {
     setStatus("Cell not empty!", "#ff7f7f");
@@ -48,18 +80,8 @@ var chkEmpty = function(cell) {
   return true;
 }
 
-var getCell = function(i, j) {
-  if ((i === 0) && (j === 0)) return $("#cell_00");
-  if ((i === 0) && (j === 1)) return $("#cell_01");
-  if ((i === 0) && (j === 2)) return $("#cell_02");
-  if ((i === 1) && (j === 0)) return $("#cell_10");
-  if ((i === 1) && (j === 1)) return $("#cell_11");
-  if ((i === 1) && (j === 2)) return $("#cell_12");
-  if ((i === 2) && (j === 0)) return $("#cell_20");
-  if ((i === 2) && (j === 1)) return $("#cell_21");
-  if ((i === 2) && (j === 2)) return $("#cell_22");
-}
-
+// Set the value of each cell and determine the number of empty cells
+// for use in checking for a win by either player.
 var updStatus = function() {
   cells = [[-1, -1, -1],[-1, -1, -1],[-1, -1, -1]];
   emptyCount = 0;
@@ -137,18 +159,7 @@ var updStatus = function() {
   }
 }
 
-var setStatus = function(msg, color) {
-  $("#statusBar").css("background-color",color);
-  $("#statusBar").html(msg);
-  $("#statusBar").show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0);
-}
-
-var setStatusAndClear = function(msg, color) {
-  $("#statusBar").css("background-color",color);
-  $("#statusBar").html(msg);
-  $("#statusBar").show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0).delay(100).show(0).delay(500).hide(0, clearBoard);
-}
-
+// Check for a win by either player.
 var chkWin = function() {
   updStatus();
   
@@ -221,12 +232,13 @@ var chkWin = function() {
   }
 }
 
+// Check if the human wants to play again
 var chkAgain = function() {
   $("#playAgain").delay(2000).fadeIn(500);
   $("#board").delay(2000).fadeOut(500);
 
   $("#No").on("click", function() {
-    $("#msg").html("OK.  Come again another time.");
+    $("#msg").html("OK.  Thank you for playing.  Come again another time.");
     $("#playAgain").fadeOut(500);
   });    
 
@@ -256,6 +268,7 @@ var evaluatePosition = function(board, p)    {
     return sum;
 }
 
+// Let the computer determine its next move.  This is the heart of the program
 var myMove = function() {
   var turn = plays + 1;
   var i, k, heuristic = -10000, utility, best = 0, worst, tmp;
@@ -263,36 +276,45 @@ var myMove = function() {
   var opponent = (turn & 1) ? 'O' : 'X';
   var board = [];
 
-  console.log("player: " + player)
-  console.log("opponent: " + opponent)
 
   if (!gameOver) {
     // Fill linear array "board" from displayed board to use for calculating best move
+    for (i=0; i<9; i++) board[i] = 2;
+
+    if ($("#cell_00").html() === '<img src="resources/X.png">') board[0] = 3;
+    if ($("#cell_01").html() === '<img src="resources/X.png">') board[1] = 3;
+    if ($("#cell_02").html() === '<img src="resources/X.png">') board[2] = 3;
+    if ($("#cell_10").html() === '<img src="resources/X.png">') board[3] = 3;
+    if ($("#cell_11").html() === '<img src="resources/X.png">') board[4] = 3;
+    if ($("#cell_12").html() === '<img src="resources/X.png">') board[5] = 3;
+    if ($("#cell_20").html() === '<img src="resources/X.png">') board[6] = 3;
+    if ($("#cell_21").html() === '<img src="resources/X.png">') board[7] = 3;
+    if ($("#cell_22").html() === '<img src="resources/X.png">') board[8] = 3;
+
+    if ($("#cell_00").html() === '<img src="resources/O.png">') board[0] = 5;
+    if ($("#cell_01").html() === '<img src="resources/O.png">') board[1] = 5;
+    if ($("#cell_02").html() === '<img src="resources/O.png">') board[2] = 5;
+    if ($("#cell_10").html() === '<img src="resources/O.png">') board[3] = 5;
+    if ($("#cell_11").html() === '<img src="resources/O.png">') board[4] = 5;
+    if ($("#cell_12").html() === '<img src="resources/O.png">') board[5] = 5;
+    if ($("#cell_20").html() === '<img src="resources/O.png">') board[6] = 5;
+    if ($("#cell_21").html() === '<img src="resources/O.png">') board[7] = 5;
+    if ($("#cell_22").html() === '<img src="resources/O.png">') board[8] = 5;
+
     if (turn === 1) {
-      best = 0;
+      // If moving first, ignore the heuristics and take a corner.  I pick a
+      // corner randomly to mix it up a bit.
+
+      var temp = Math.trunc((Math.random() * 4));
+
+      switch(temp) {
+        case 0: best = 0;break;
+        case 1: best = 2;break;
+        case 2: best = 6;break;
+        case 3: best = 8;break;
+      }
+
     } else {
-      for (i=0; i<9; i++) board[i] = 2;
-
-      if ($("#cell_00").html() === '<img src="resources/X.png">') board[0] = 3;
-      if ($("#cell_01").html() === '<img src="resources/X.png">') board[1] = 3;
-      if ($("#cell_02").html() === '<img src="resources/X.png">') board[2] = 3;
-      if ($("#cell_10").html() === '<img src="resources/X.png">') board[3] = 3;
-      if ($("#cell_11").html() === '<img src="resources/X.png">') board[4] = 3;
-      if ($("#cell_12").html() === '<img src="resources/X.png">') board[5] = 3;
-      if ($("#cell_20").html() === '<img src="resources/X.png">') board[6] = 3;
-      if ($("#cell_21").html() === '<img src="resources/X.png">') board[7] = 3;
-      if ($("#cell_22").html() === '<img src="resources/X.png">') board[8] = 3;
-
-      if ($("#cell_00").html() === '<img src="resources/O.png">') board[0] = 5;
-      if ($("#cell_01").html() === '<img src="resources/O.png">') board[1] = 5;
-      if ($("#cell_02").html() === '<img src="resources/O.png">') board[2] = 5;
-      if ($("#cell_10").html() === '<img src="resources/O.png">') board[3] = 5;
-      if ($("#cell_11").html() === '<img src="resources/O.png">') board[4] = 5;
-      if ($("#cell_12").html() === '<img src="resources/O.png">') board[5] = 5;
-      if ($("#cell_20").html() === '<img src="resources/O.png">') board[6] = 5;
-      if ($("#cell_21").html() === '<img src="resources/O.png">') board[7] = 5;
-      if ($("#cell_22").html() === '<img src="resources/O.png">') board[8] = 5;
-
       for(k = 0; k < 9; k++)  {
         if(board[k] === 2) { // found a blank square
           board[k] = (turn & 1) ? 3 : 5;  // try playing this move
@@ -326,6 +348,8 @@ var myMove = function() {
 
     plays++;
 
+    // Place the selected play on the board and then check for a win.
+
     switch (best) {
       case 0: $("#cell_00").html(compIcon);break;
       case 1: $("#cell_01").html(compIcon);break;
@@ -348,6 +372,7 @@ var myMove = function() {
 $(document).ready( function() {
   "use strict";
 
+  // Let the player select "X", or "O"
   $("#playerChoice").fadeIn(500);
 
   $("#X").on("click", function() {
@@ -363,6 +388,11 @@ $(document).ready( function() {
     $("#playerChoice").fadeOut(500);
     $("#board").fadeIn(500, myMove());
   });  
+
+  // Wait for the human player to select a move.
+  // Verifiy that the move is legal and check for a win if it is.
+  // If the move is not legal (the cell is not empty), inform the player
+  // and wait for another move.
 
   $("#cell_00").on("click", function() {
     if (chkEmpty($("#cell_00"))) {
